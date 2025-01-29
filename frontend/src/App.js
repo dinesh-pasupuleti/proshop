24f2f185a19b039cbea071f85ffe3c7666b67085
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2025-01-29 11:38:35 -0500

    serving, fetching data & MongoDB conection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import React from 'react';
+import Footer from "./components/Footer";
+import {Outlet} from 'react-router-dom'
 const App = () => {
   return (
-    <h1>Welcome To Proshop</h1>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+       <Outlet />
+      </Container>
+    </main>  
+    <Footer/>
+    </>
   )
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {
+    const currentUYear = new Date().getFullYear()
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                    <p>ProShop &copy; {currentUYear}</p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer;
