8ca6b1a773d723246a00d14ab1d7878b3bbb5b64
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2025-01-31 16:30:32 -0500

    implemenetd redux state management

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
-
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error?.error || 'An error occurred'}</div>
+        <Message variant='danger'>{error?.data?.message || error?.error || 'An error occurred'}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
