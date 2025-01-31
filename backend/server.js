2740a8f48930d4d89d326d9354683670bbe470d5
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2025-01-31 10:54:11 -0500

    fetching data from mongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
 const port = process.env.PORT || 5001;
+import productRoutes from './routes/productRoutes.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 
 connectDB(); // to connect MOngoDB
@@ -13,18 +14,15 @@ const app = express();
 
 app.use(cors());
 
+
 app.get('/', (req, res) => {    
     res.send('API is  running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
