bbfadbc3fa79b4d091315b33794e5243ed06edea
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2025-01-31 15:33:09 -0500

    products api slice

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products'
 import { Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card,Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
